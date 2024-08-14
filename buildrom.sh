53042ca499f59daf19dd8324991a1fa658bed35b
OrdinaryEnder <zairullahfreefire@outlook.com>
2024-08-14 21:23:09 +0700

    Update buildrom.sh

--- buildrom.sh
+++ buildrom.sh
@@ -21,11 +21,11 @@ git config --global user.email "root@localhost"
 git config --global user.name "Who am i?"
 git config --global color.ui true
 cd ~/android
-repo init -u https://github.com/Gofaraway71/android_manifest -b 13 --git-lfs
-repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
-git clone https://github.com/Task-Force-Team/device_xiaomi_camellia/ -b thirteen ~/android/device/xiaomi/camellia
-git clone https://github.com/dm700-devs/device_xiaomi_camellia-kernel -b thirteen ~/android/kernel/xiaomi/camellia
-git clone https://github.com/dm700-devs/vendor_xiaomi_camellia -b thirteen ~/android/vendor/xiaomi/camellia
+repo init -u https://github.com/ProjectSakura/android -b 13 --git-lfs
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle
+git clone https://github.com/Task-Force-Team/device_xiaomi_camellia/ -b fourteen ~/android/device/xiaomi/camellia
+git clone https://github.com/dm700-devs/device_xiaomi_camellia-kernel -b fourteen ~/android/kernel/xiaomi/camellia
+git clone https://github.com/dm700-devs/vendor_xiaomi_camellia -b fourteen ~/android/vendor/xiaomi/camellia
 source build/envsetup.sh
 breakfast camellia
 export USE_CCACHE=1
